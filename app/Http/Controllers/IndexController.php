75b26de8af4ad130e458ebe9bf42601b1de156a6
Студент Академии <std@itstep.loc>
2025-08-11 21:39:48 +0300

    Начали верстку

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,6 +12,7 @@ class IndexController extends Controller
     {
         $books = Book::existBooks()->get();
         $authors = Author::whoseBooksExist()->get();
-    dd($books, $authors);
+
+        return view('index', compact('books', 'authors'));
     }
 }
