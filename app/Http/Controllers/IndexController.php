a9689937b6a9f3801b9457b13ea72615fd9896fc
Студент Академии <std@itstep.loc>
2025-08-25 20:48:18 +0300

    Прокинули переменную корзины через сервис-провайдер

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
 {
-    public function index() 
+    public function index()
     {
         $books = Book::existBooks()->get();
         $authors = Author::whoseBooksExist()->get();
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::share(
+            'cart',
+            Cart::query()
+            ->where('user_id', Auth::id())
+            ->first()
+        );
     }
 }
